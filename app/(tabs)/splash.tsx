526c8215913976cc8233497107f1f990c4f1c548
wgreunke <wgreunke@gmail.com>
2025-08-20 08:10:39 -0700

    Stopping for now.

--- app/(tabs)/splash.tsx
+++ app/(tabs)/splash.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { router } from 'expo-router';
 
 export default function Splash() {
     return (
         <View>
-            <Text>Splash</Text>
+            <Text>Welcome to the AWS Simulator</Text>
+            <Text>Please Note:</Text>
+            <Text>1) ???? Is not affiliated with or endorsed by AWS</Text>
+            <Text>2) All data is deleted when you close the app</Text>
+            <Text>3) Uploaded files are stored locally, not shared and removed from the app when you close it.</Text>
+            <Button title="Lets Go!" onPress={() => {
+                router.push('/');
+            }} />
+
         </View>
     );
 }
